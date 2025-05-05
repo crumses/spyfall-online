6ff0e11778dfbff6eba6aa4c3c75657c8aedebd4
crumses <eyupyilmazirl@gmail.com>
2025-05-05 15:31:44 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io();
+const socket = io(window.location.origin);
 
 function App() {
   const [username, setUsername] = useState("");
