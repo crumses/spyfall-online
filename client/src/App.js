34f04e1e0d7d91191b2a3ea3adb3d48f22717245
crumses <eyupyilmazirl@gmail.com>
2025-05-05 15:22:39 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:3001");
+const socket = io(); // Render'da aynı domaine bağlanır, en güvenlisi budur.
 
 function App() {
   const [username, setUsername] = useState("");
